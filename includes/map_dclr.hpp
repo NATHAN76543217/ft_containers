32d1b77f21d1d47f544adea725e22415681a1da6
Nathan <maximenathan@hotmail.fr>
2021-10-18 01:39:36 +0200

    map: add declaration for construct/copy/destroy and all typedefs

--- includes/map_dclr.hpp
+++ includes/map_dclr.hpp
@@ -40,6 +40,7 @@ namespace ft {
 		        value_compare(key_compare c);
 		    public:
 		        bool operator()(const value_type& x, const value_type& y) const;
+				//TODO verifier attentivement ce qu'il y a à faire ici
 		};
 			
 		class iterator
@@ -297,12 +298,21 @@ namespace ft {
 
 		typedef ft::reverse_iterator<iterator>			reverse_iterator;
 		typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
-			
-		explicit map(const allocator_type& alloc = allocator_type());
-		explicit map(size_t n, const value_type& value = value_type(), const allocator_type& alloc = allocator_type());
-	//TODO add range constructors
-			map( const map & src );
-			~map();
+
+	/*
+	** ------------------------------- CONSTUCT/COPY/DESTROY --------------------------------
+	*/	
+		explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type());
+		template <class InputIterator>
+			map(InputIterator first, InputIterator last,
+				const key_compare& comp = key_compare(),
+				const allocator_type& alloc = allocator_type());
+		map(const map& src);
+		~map();
+
+    	map& operator=(const map& m);
+
+
 		map						&operator=( const map& other );
 
 
@@ -351,7 +361,7 @@ namespace ft {
 	** ------------------------------- ALLOCATORS --------------------------------
 	*/
 
-			allocator_type			get_allocator() const;
+		allocator_type				get_allocator() const;
 	/*
 	** ------------------------------- OBSERVERS --------------------------------
 	*/
@@ -416,92 +426,11 @@ namespace ft {
 		x.swap(y);
 	}
 
-}
+};
 
 #endif /* ********************************************************** MAP_DCLR_HPP */
 
 /*
 ** Notes
-** - map capacity is doubled each time
-** - map capacity start at 0 for default constructor
 */
-
-template <class Key, class T, class Compare = less<Key>,
-          class Allocator = allocator<pair<const Key, T>>>
-class map
-{
-public:
-    // types:
-    typedef Key                                      key_type;
-    typedef T                                        mapped_type;
-    typedef pair<const key_type, mapped_type>        value_type;
-    typedef Compare                                  key_compare;
-    typedef Allocator                                allocator_type;
-    typedef typename allocator_type::reference       reference;
-    typedef typename allocator_type::const_reference const_reference;
-    typedef typename allocator_type::pointer         pointer;
-    typedef typename allocator_type::const_pointer   const_pointer;
-    typedef typename allocator_type::size_type       size_type;
-    typedef typename allocator_type::difference_type difference_type;
-
-    typedef implementation-defined                   iterator;
-    typedef implementation-defined                   const_iterator;
-    typedef std::reverse_iterator<iterator>          reverse_iterator;
-    typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
-
-    class value_compare : public std::binary_function<value_type, value_type, bool>
-    {
-        friend class map;
-    protected:
-        key_compare comp;
-
-        value_compare(key_compare c);
-    public:
-        bool operator()(const value_type& x, const value_type& y) const;
-    };
-
-    // construct/copy/destroy:
-    map()
-        noexcept(
-            is_nothrow_default_constructible<allocator_type>::value &&
-            is_nothrow_default_constructible<key_compare>::value &&
-            is_nothrow_copy_constructible<key_compare>::value);
-    explicit map(const key_compare& comp);
-    map(const key_compare& comp, const allocator_type& a);
-    template <class InputIterator>
-        map(InputIterator first, InputIterator last,
-            const key_compare& comp = key_compare());
-    template <class InputIterator>
-        map(InputIterator first, InputIterator last,
-            const key_compare& comp, const allocator_type& a);
-    map(const map& m);
-    map(map&& m)
-        noexcept(
-            is_nothrow_move_constructible<allocator_type>::value &&
-            is_nothrow_move_constructible<key_compare>::value);
-    explicit map(const allocator_type& a);
-    map(const map& m, const allocator_type& a);
-    map(map&& m, const allocator_type& a);
-    map(initializer_list<value_type> il, const key_compare& comp = key_compare());
-    map(initializer_list<value_type> il, const key_compare& comp, const allocator_type& a);
-   ~map();
-
-    map& operator=(const map& m);
-    map& operator=(map&& m)
-        noexcept(
-            allocator_type::propagate_on_container_move_assignment::value &&
-            is_nothrow_move_assignable<allocator_type>::value &&
-            is_nothrow_move_assignable<key_compare>::value);
-    map& operator=(initializer_list<value_type> il);
-
-
-
-
-
-
-
-
-
-
-};
 
