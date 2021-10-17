d1acad44cea07e3f95d207aeb7a91f122081a366
Nathan <maximenathan@hotmail.fr>
2021-10-18 00:14:25 +0200

    global: add pair and make_pair

--- includes/base.hpp
+++ includes/base.hpp
@@ -97,6 +97,55 @@ namespace ft {
 		}
 		return (first1 == last1) && (first2 != last2);	
 	}
+	template<class T1, class T2> struct pair
+	{
+		typedef T1			first_type;
+		typedef T2			second_type;
+
+		T1		first;
+		T2		second;
+		
+		pair() : first(), second() { }
+		pair(const T1& a, const T2& b) : first(a), second(b) { }
+		template<class U1, class U2>
+        pair(const pair<U1, U2>& p) : first(p.first), second(p.second) { }
+    };
+
+	  /// Two pairs of the same type are equal iff their members are equal.
+	template<class T1, class T2>
+		inline bool
+		operator==(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{ return x.first == y.first && x.second == y.second; }
+
+	template<class T1, class T2>
+		inline bool
+		operator<(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{ return x.first < y.first
+			|| (!(y.first < x.first) && x.second < y.second); }
+
+	template<class T1, class T2>
+		inline bool
+		operator!=(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{ return !(x == y); }
+
+	template<class T1, class T2>
+		inline bool
+		operator>(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{ return y < x; }
+
+	template<class T1, class T2>
+		inline bool
+		operator<=(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{ return !(y < x); }
+
+	template<class T1, class T2>
+		inline bool
+		operator>=(const pair<T1, T2>& x, const pair<T1, T2>& y)
+		{ return !(x < y); }
+
+	template< class T1, class T2 >
+		inline pair<T1,T2> make_pair( T1 t, T2 u )
+		{ return pair<T1, T2>(t, u); };
 }
 
 #endif              //BASE_HPP
