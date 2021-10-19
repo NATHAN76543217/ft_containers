56aadda9028450532f555a5ca3278a0e1769ae63
Nathan Lecaille <nlecaill@z4r6p3.42lyon.fr>
2021-10-19 05:13:38 +0200

    RBT: implement insertion

--- mainRBT.cpp
+++ mainRBT.cpp
@@ -1,9 +1,11 @@
 #include "RBTree.hpp"
 
 int main(){
-    RBTree<std::string> tree;
-    RBTree<int> treeInt;
+    ft::RBTree<std::string> tree;
+    ft::RBTree<int> treeInt;
     std::string mystring("1st element");
+
+    std::cout << "TEST STRING" << std::endl;
     tree.print();
     tree.insert(mystring);
     try {
@@ -12,14 +14,21 @@ int main(){
         std::cout << "catch:" << e.what() << "\n";
     }
     tree.print();
+    tree.insert("Uno");
+    tree.insert("Due");
+    tree.insert("Tre");
+    tree.insert("Quatro");
+    tree.print();
 
     treeInt.insert(76);
     treeInt.insert(101);
     treeInt.insert(16);
     treeInt.insert(90);
- 
     treeInt.insert(85);
     treeInt.insert(65);
+    treeInt.print();
+
+    std::cout << "TEST INT" << std::endl;
     treeInt.insert(55);
     treeInt.insert(20);
     treeInt.insert(71);
@@ -30,18 +39,16 @@ int main(){
     treeInt.insert(61);
     treeInt.print();
 
-    // treeInt.insert(7);
-    // treeInt.insert(6);
-    // treeInt.insert(9);
-    // treeInt.print();
-    // treeInt.insert(8);
-    // treeInt.print();
-    // treeInt.insert(2);
-    // treeInt.print();
-    // treeInt.insert(3);
-    // treeInt.insert(1);
-    // treeInt.print();
+    treeInt.insert(7);
+    treeInt.insert(6);
+    treeInt.insert(9);
+    treeInt.insert(8);
+    treeInt.insert(2);
+    treeInt.insert(3);
+    treeInt.insert(1);
+    treeInt.print();
 
-
+    std::cout << "TEST PAIR" << std::endl;
+    //TODO implement test with pair
     return 0;
 }
